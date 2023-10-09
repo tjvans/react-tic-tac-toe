684ed0b848b89cc4864a0e9888336dc67a41ec9f
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-09 15:57:31 +0200

    lift state from square to board parent component

--- react-vite-first/src/components/Square.jsx
+++ react-vite-first/src/components/Square.jsx
@@ -1,17 +1,3 @@
-import {useState} from "react"
-
-export default function Square() {
-    const [value, setValue] = useState(null)
-
-    function handleClick() {
-        setValue("X")
-    }
-    
-    return (
-        <button 
-            className="square"
-            onClick={handleClick}>
-            {value}
-        </button>
-    )
+export default function Square({value}) {
+    return <button className="square">{value}</button>
 }
