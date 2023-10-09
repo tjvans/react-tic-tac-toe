8963a5202d030854852807af0f4118765b54deca
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-09 14:44:39 +0200

    remove value prop and add state to square component

--- react-vite-first/src/components/Square.jsx
+++ react-vite-first/src/components/Square.jsx
@@ -1,3 +1,17 @@
-export default function Square({value}) {
-    return <button className="square">{value}</button>
+import {useState} from "react"
+
+export default function Square() {
+    const [value, setValue] = useState(null)
+
+    function handleClick() {
+        console.log('Clicked!')
+    }
+    
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}>
+            {value}
+        </button>
+    )
 }
