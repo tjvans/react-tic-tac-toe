8b2f74664d6e7d730e8245cccf12c232c0f89580
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-10 12:09:22 +0200

    implement move history navigation

--- react-vite-first/src/components/Game.jsx
+++ react-vite-first/src/components/Game.jsx
@@ -4,15 +4,19 @@ import Board from "./Board.jsx";
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true)
     const [history, setHistory] = useState([Array(9).fill(null)])
-    const currentSquares = history[history.length-1]
+    const [currentMove, setCurrentMove] = useState(0)
+    const currentSquares = history[currentMove]
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares])
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory)
+        setCurrentMove(nextHistory.length - 1)
         setXIsNext(!xIsNext)
     }
 
-    function jumptTo() {
-        // TODO
+    function jumptTo(nextMove) {
+        setCurrentMove(nextMove)
+        setXIsNext(nextMove % 2 === 0)
     }
 
     const moves = history.map((squares, move) => {
