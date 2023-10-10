436237c2a4a3aea2ad58157692bb108eb85512f0
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-10 11:46:14 +0200

    track move history and display history buttons

--- react-vite-first/src/components/Game.jsx
+++ react-vite-first/src/components/Game.jsx
@@ -11,13 +11,32 @@ export default function Game() {
         setXIsNext(!xIsNext)
     }
 
+    function jumptTo() {
+        // TODO
+    }
+
+    const moves = history.map((squares, move) => {
+        let description
+        if (move > 0) {
+            description = "Go to move #" + move
+        } else {
+            description = "Go to game start"
+        }
+
+        return (
+            <li>
+                <button onClick={() => jumptTo(move)}>{description}</button>
+            </li>
+        )
+    })
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     )
